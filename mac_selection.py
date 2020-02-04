978a34db9cb48fa1d816b435879600c6c18a80aa
Rohit Kumar <rokumar@ucsd.edu>
2020-02-03 17:21:11 -0800

    Initial commit

---
+++ mac_selection.py
@@ -0,0 +1,16 @@
+# %%
+# Add imports
+from mac_selection import *
+# %%
+
+
+def DynaMAC(perf_metric_1, perf_metric_2, perf_metric_3):
+    """ML function for MAC selection.
+
+    Arguments:
+        perf_metric_1 {[type]} -- [description]
+        perf_metric_2 {[type]} -- [description]
+        perf_metric_3 {[type]} -- [description]
+    """
+    pass
+    return
---
+++ mac_simulator.py
@@ -0,0 +1,19 @@
+# %%
+# Add imports
+# %%
+
+
+def simulator(num_nodes=10, num_packets=10, num_slots=10, sim_end_time=10):
+    """Function for implementing the event based simulator.
+
+    Keyword Arguments:
+        num_nodes {int} -- Number of nodes in the simulation. (default: {10})
+        num_packets {int} -- Number of packets at each node. Time/event instant at which packet must become available must be random/programmable. (default: {10})
+        num_slots {int} -- If slot based structure is being used for MAC protocol, number of slots. (default: {10})
+        sim_end_time {int} -- [description] (default: {10})
+
+    Returns:
+        [type] -- [description]
+    """
+
+    return perf_metric_1, perf_metric_2, perf_metric_3
