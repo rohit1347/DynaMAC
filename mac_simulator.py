cedf597c5086cc21556e8b013ec9d58d4fb10dd0
Rohit Kumar <rokumar@ucsd.edu>
2020-02-08 17:47:03 -0800

    Update CSMA simulator

--- mac_simulator.py
+++ mac_simulator.py
@@ -22,24 +22,29 @@ def simulator(num_nodes=10, num_packets=3, num_slots=10, sim_end_time=10):
     assert num_slots > 0
     assert sim_end_time > 0
     # Variables
-    state_id = [0, 0]
+    state_id = [0, [0]]
     # state_id values - 0: carrier sense, 1: transmitting, 2: tx end
+    packet_time = 5
+    # packet time units - seconds
+
     # Program
 
     events = generate_events(
         num_nodes=num_nodes, num_packets=num_packets, sim_end_time=sim_end_time)
+    simEvents = update_event_counter(events=simEvents)
     print(f'SimEvents={simEvents}')
-    simEvents = update_event_counter(events=events)
     for event_id, event in simEvents:
-        if state_id[0] = 0:
-            simEvents[]
-        elif state_id = 1:
+        if state_id[0] == 0:
+            state_id[1] = event+packet_time
+            state_id[0] = 1
+            simEvents = np.delete(simEvents, event_id)
+        elif state_id == 1:
             pass
-        elif state_id = 2:
+        elif state_id == 2:
             pass
         else:
             print("Wrong AP state")
-
+        simEvents = update_event_counter(events=simEvents)
     print('Simulation has completed.')
     return events
     # return perf_metric_1, perf_metric_2, perf_metric_3
@@ -55,11 +60,12 @@ def generate_events(num_nodes=10, num_packets=3, sim_end_time=10):
     return events
 
 
-def update_event_counter(events=generate_events()):
-    simEvents = np.unique(events)
+def update_event_counter(events=generate_events(), end_time=10):
+    events = np.unique(events, axis=0)
+    events.sort()
     # By putting axis=0, we find unique numbers across each row.
     # Expected to preserve shape of the input.
-    return simEvents
+    return events
 
 
 def generate_backoff():
