8a59d8ba2ccc6ca1acb989b13e94d6e3a11d699f
Rohit Kumar <rokumar@ucsd.edu>
2020-03-07 18:19:04 -0800

    Corrected backoffs and latency calculation

--- csma_test.py
+++ csma_test.py
@@ -1,12 +1,8 @@
 # %% adding imports
 from mac_simulator import *
 
+# %%
+_, _, _, lat = CSMA_simulator(duration=100, packet_time=0.01)
+
 # %% Generating plots
 generate_xput_plots(montecarlo=2, num_n_end=10, num_p=2, duration=1e6)
-
-
-# %%
-CSMA_simulator(duration=1e6, packet_time=0.01)
-
-
-# %%
