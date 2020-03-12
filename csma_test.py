8d3abc20734b425ef02b9c4f8361c60f2c359218
yeswanth2019 <yguntupa@ucsd.edu>
2020-03-12 10:38:54 -0700

    made changes to qlearningegrredy code

--- csma_test.py
+++ csma_test.py
@@ -2,13 +2,13 @@
 from mac_simulator import *
 from tdma_simulator import *
 from mac_selection import *
-
-
-# %%
-print(DynaMAC_switch_test(num_n=30, num_p=100))
+from qlearning_optimized import *
 
 # %%
-_, xput, _, lat = CSMA_simulator(duration=500, packet_time=0.01)
+latency_final,xput_final,prot_arr = DynaMAC_somac_test(num_n=30, num_p=10,duration=20)
+
+# %%
+#_, xput, _, lat = CSMA_simulator(duration=500, packet_time=0.01)
 
 # %% Generating plots
 generate_xput_plots(montecarlo=2, num_n_end=10, num_p=2, duration=1e6)
