a501c8dbb3ab9d04a2c6f5e98d8e68b3e324c730
ankit2579 <56931694+ankit2579@users.noreply.github.com>
2020-03-11 18:58:46 -0700

    added softmax code
    Added the code for boltzmann based softmax. When mode is set to 3, softmax approach will be adopted.

--- qlearning_optimized.py
+++ qlearning_optimized.py
@@ -8,6 +8,78 @@ from matplotlib.legend_handler import HandlerLine2D
 
 g_dt = -2
 
+class QLearningBoltzmann:
+
+	def __init__(self, prot, learn_rate = 0.3, discount = 0.8, T = 0.1):
+		self.q_table   = np.zeros((2, 2))
+		
+		self.prob_table = np.zeros((2, 2)) + 0.5
+		self.discount   = discount
+		self.learn_rate = learn_rate
+		self.reward     = 0.
+
+		self.T          = T
+
+		seed = int(time.time())
+		np.random.seed(seed)
+
+		_ = self.decision(prot, keep = True)
+		
+		logging.info("QLearnging Boltzmann")
+		logging.info("T = {}, learn_rate = {}, discount factor = {}".format(self.T, self.learn_rate, self.discount))
+
+		return
+		
+	def decision(self, prot, keep = False, force_switch = False):
+		self.state = prot
+
+		if force_switch == True:
+			action = 0 if prot == 1 else 1
+			action1.append(action)
+		elif keep == False:
+			action = np.random.choice(
+					np.array([0, 1]),
+					p = self.prob_table[self.state]
+				)
+			action1.append(action)
+			#prob.append(self.prob_table[self.state])
+		else:
+			action = prot
+			action1.append(action)
+
+		logging.info("Choice = {}".format(action))
+		
+		self.action = action
+		self.state_new = action
+		
+		return action
+
+	def update_qtable(self, reward, dt):
+		self.q_table[self.state, self.action] = (1. - self.learn_rate) * self.q_table[self.state, self.action] + \
+						self.learn_rate * (reward + self.discount * np.max(self.q_table[self.state_new, :]))
+
+		T = self.T
+		num = np.exp(self.q_table / T)
+
+		sum_cols = np.sum(num, 1)
+		for row in range(num.shape[0]):
+			num[row, :] = num[row, :] / sum_cols[row]
+
+		self.prob_table = num
+
+		logging.info("Temperature = {}".format(T))
+		logging.info("Reward = {}".format(reward))
+		logging.info("QTable = \n{}".format(self.q_table))
+		logging.info("Prob Table = \n{}".format(self.prob_table))
+
+		return
+
+	def reset_qtable(self):
+		self.q_table   = np.zeros((2, 2))
+		self.prob_table = np.zeros((2, 2)) + 0.5
+
+		return
+
 class QLearningEGreedy:
 
 	def __init__(self, prot, learn_rate = 0.3, discount = 0.8, epsilon = 0):
@@ -113,7 +185,9 @@ class decision_final:
 
 		# ML modules
         if mode == 2:
-			somac = QLearningEGreedy(protid)
+		somac = QLearningEGreedy(protid)
+	if mode == 3
+		somac = QlearningBoltzmann(protid)
         if np.any(np.equal(metric, None)) == False:
 			g_dt = g_dt + 1
             if ((mode == 2) and (metric.shape>=4)): 
